e20ca9866008dd26dbd8eb4e67ce27ac756ec4ff
vittoric <toricod22@gmail.com>
2024-09-21 12:21:18 +0200

    navbar done

---
+++ src/app/components/Navbar/Navbar.tsx
@@ -0,0 +1,51 @@
+"use client";
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+
+export const Navbar: React.FC = () => {
+    const [navbarBg, setNavbarBg] = useState(false);
+
+    const changeBackground = () => {
+        if (window.scrollY >= 80) {
+            setNavbarBg(true);
+        } else {
+            setNavbarBg(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', changeBackground);
+        return () => {
+            window.removeEventListener('scroll', changeBackground);
+        };
+    }, []);
+
+    return (
+        <nav
+      className={`fixed top-0 w-full h-20 z-50 transition-colors duration-300 ease-in-out ${
+        navbarBg ? 'bg-primaryLila' : 'bg-transparent'
+      }`}>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between items-center h-16 mt-2">
+          <div>
+            <a href="#">
+              <Image src="/logo-blanc.png" alt="Logo_nucleo" width={150} height={50} />
+            </a>
+          </div>
+          <div className="flex items-center space-x-8">
+            <a href="#" className="text-white text-lg hidden sm:block">
+              Planes
+            </a>
+            <a
+              href="#"
+              className="border border-white text-white px-4 py-2  hover:bg-white hover:text-gray-900 transition duration-300">
+              Conversemos <span className="inline-block transform rotate-45">↑</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+    );
+};
+
+export default Navbar;
